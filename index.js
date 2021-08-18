97ea7c04454a2025537dc31ccbc5739f9fe2733c
Nick <nick@heroforge.com>
2021-08-17 18:10:50 -0700

    add error handling and pretty print

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const data = require('./property_listings.json');
 var args = process.argv.slice(2);
 
-// if (args.length) {
-//     console.log("Please provide stateAbbrviation and minBaths as command line arguments.")
-// }
+const VALID_STATES = {
+    'CA': true,
+    'FL': true,
+    'NY': true,
+}
+
+if (!VALID_STATES[args[0]]) {
+    console.log("Please enter a valid state as your first argument.");
+    process.exitCode = 1;
+}
+
+if (isNaN(Number(args[1])) || Number(args[1]) < 0) {
+    console.log("Please enter an integer >=0 as your second argument.");
+    process.exitCode = 1;
+}
+
+if (process.exitCode) process.exit();
 
 const stateAbbreviation = args[0];
 const minBaths = args[1];
 
 let filteredHouses = data.filter((house) => {
+    if (!house.price || !house.squareFeet) return false;
     return stateAbbreviation === house.administrativeAreaLevel1 && minBaths <= house.baths;
 }).sort(( a, b ) => {
     return a.squareFeet - b.squareFeet;
@@ -26,34 +41,28 @@ let formattedHouses = filteredHouses.map((house) => {
     }
 })
 
-console.log(formattedHouses);
+console.log(JSON.stringify(formattedHouses, null, 2));
 
-const calcAvgPrice = () => {
-    let totalPrice = filteredHouses.map((house) => {
-        return Math.trunc(house.price * 100);
-    }).reduce((acc, curr) => {
-        return acc + curr;
-    }, 0);
-
-    return Math.round(totalPrice / filteredHouses.length) / 100;
+const calcAvgPrice = (price) => {
+    return Math.round(price / filteredHouses.length) / 100;
 }
 
-let avgPrice = calcAvgPrice();
+let totalPrice = filteredHouses.map((house) => {
+    return Math.trunc(house.price * 100);
+}).reduce((acc, curr) => {
+    return acc + curr;
+}, 0);
+
+let avgPrice = calcAvgPrice(totalPrice);
 
 console.log(`Average Price: $${avgPrice}`)
 
-const calcAvgPricePerSqft = () => {
-    let pricesPerSqft = filteredHouses.map((house) => {
-        if (!house.price || !house.squareFeet) return null;
-        return Math.trunc(house.price * 100) / house.squareFeet;       
-    }).reduce((acc, curr) => {
-        return acc + curr;
-    }, 0);
+let totalPricePerSqft = filteredHouses.map((house) => {
+    return Math.trunc(house.price * 100) / house.squareFeet;       
+}).reduce((acc, curr) => {
+    return acc + curr;
+}, 0);
 
-    return Math.round(pricesPerSqft / filteredHouses.length) / 100;
-}
-
-
-let avgPricePerSqft = calcAvgPricePerSqft();
+let avgPricePerSqft = calcAvgPrice(totalPricePerSqft);
 
 console.log(`Average Price per sqft: $${avgPricePerSqft}`);
